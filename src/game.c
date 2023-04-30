d350a44339d4cc240c986f1f944f484715fd458b
Adam Terrak <adamterrak@gmail.com>
2023-04-30 16:03:07 +0200

    PTN LA MAP MARCHE

--- src/game.c
+++ src/game.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 #include "../headers/game.h"
 #include "../headers/structs.h"
 #include "../headers/print.h"
@@ -55,7 +56,8 @@ void menu(int* exit){
                     x=22;
                 }
                 break;
-            
+
+            case KEY_ENTER:
             case 'e':
                 switch (x)
                 {
@@ -81,99 +83,76 @@ void menu(int* exit){
         delwin(winmenu);
 }
 
-<<<<<<< HEAD
-void game(int* exit,int* x,int* y){
-    WINDOW* map=newwin(LINES,COLS,*x,*y);
-    WINDOW* affichage=subwin(map,50,170,0,0);
-
-    int Xaff,Yaff;
-    getmaxyx(affichage,Xaff,Yaff);
-
-    int ch=ERR;
-        
-    ch=getch();
-            
-    switch (ch)
-    {
-    case 's':
-        wclear(map);
-        *x=*x-1;
-        
-        break;
-
-    case 'z':
-        wclear(map);
-        *x=*x+1;
-        
-        break;
-
-    case 'q':
-        wclear(map);
-        *y=*y+2;
-
-        break;
-
-    case 'd':
-        wclear(map);
-        *y=*y-2;
-
-        break;
-    
-    case 'm':
-        menu(exit);
-        break;
-
-    case 'p':
-        *exit=1;
-        break;
-    default:
-        break;
-    }
-
-    box(map,0,0);
-    box(affichage,0,0);
-    print_player(affichage,Xaff/2-3,Yaff/2-4);
-    create_map(map);
-    wrefresh(map);
-=======
 void game(int* exit,int* l,int* c){
     int ch=ERR;int i;int j;
-    WINDOW* map = newwin(100+LINES/2+LINES/2, 350+COLS/2+COLS/2, 0, 0);
+    struct timespec trm, trq = { 0,10000000 };
+    WINDOW* map = newwin(170,500, 0, 0);
     WINDOW* cam=subwin(map,LINES-1,COLS-1,*l,*c);
+    WINDOW* fenetre_backup= newwin(170, 500, 0, 0);
     box(map,0,0);
-    box(cam,0,0);
-    
-       mvwprintw(cam,LINES/2,COLS/2,"@");
-                ch=getch();
-                switch (ch)
-                {
-                case KEY_UP:
-                case 'z':
-                 *l=*l-1;
-                    break;
-                case KEY_DOWN:
-                case 's':
+    box(fenetre_backup,0,0);
+
+    for(i=((LINES-2)/2);i<169-((LINES-2)/2);i++){ /*i=abscisse de @ dans map d'affichage +1; i< nombre lignes map - abscisse de @ dans fenêtre d'affichage -1*/
+        mvwprintw(map,i,1,"                                                                                                                             ");/*"ordonnée de @ dans map d'affichage" d'espaces*/
+        for(j=126;j<500-((COLS-2)/2);j++){/*j= ordonnée qui suit;j< nombre colonnes fenêtre - ordonnée de @ dans fenêtre d'affichage -1 */
+          mvwprintw(map,i,j," ");
+        }
+    }
+
+    fenetre_backup=dupwin(map);
+
+    mvwin(cam,0,0);
+    mvwprintw(cam,(LINES-2)/2,(COLS-2)/2,"@"); //10,40
+
+    ch=getch();
+    switch (ch)
+    {
+        case KEY_UP:
+        case 'z':
+            if (*l!=0)
+            {
+                *l=*l-1;
+            }
+            break;
+        case KEY_DOWN:
+        case 's':
+            if (*l!=108)
+            {
                 *l=*l+1;
-                    break;
-                case KEY_RIGHT:
-                case 'd':
-                  *c=*c+1;
-                    break;
-                case KEY_LEFT:
-                case 'q':
-                  *c=*c-1;
-                    break;
-                case 'm':
-                menu(exit);
-                case 'p':
-                *exit=1;
-                    break;
-                default:
-                    break;
-                }     /*pour cadrer la camera la première fois sans cadrer à chaque fois dans la boucle*/
+            }
+            
+            break;
+        case KEY_RIGHT:
+        case 'd':
+            if (*c!=265)
+            {
+                *c=*c+1;
+            }
+            
+            break;
+        case KEY_LEFT:
+        case 'q':
+            if (*c!=0)
+            {
+                *c=*c-1;
+            }
+            
+            break;
+        case 'm':
+            menu(exit);
+            break;
+
+        case 'p':
+            *exit=1;
+                break;
+
+        default:
+            break;
+    }                     /*pour cadrer la camera la première fois sans cadrer à chaque fois dans la boucle*/
     wrefresh(cam);       //affichage camera
->>>>>>> 36ba0c8d0ad3e268a80007967b1b0c145e68d204
-    sleep(1/60);
+    map=dupwin(fenetre_backup);
+    nanosleep(&trq,&trm);
     delwin(cam);
     delwin(map);
+    delwin(fenetre_backup);
 }
